30a4973d140700afe2077b7333c51030d4e03e7c
siderisng <freelook.gr@gmail.com>
2017-04-25 12:08:40 +0300

    no memory problem

--- biplex_v2.m
+++ biplex_v2.m
@@ -22,8 +22,8 @@ n2=size(A,1); % number of links
 n3=size(A2,1);
 Kout=zeros(1,n); 
 Kout2=zeros(1,n);
-Pa= zeros(n,n); % P(i,j)= 0 if i dangling, 1/Kout(i) else
-Pa2= zeros(n,n);
+Pa= spalloc(double(n),double(n),10000); % P(i,j)= 0 if i dangling, 1/Kout(i) else
+Pa2= spalloc(double(n),double(n),10000);
 e= ones(1,n);
 
 fprintf('number of nodes: %d, number of links: %d %d \n',n,n2,n3); 
@@ -44,23 +44,14 @@ end
 for i = 1:n2
     x=A(i,1);
     y=A(i,2);
-    Pa(x,y)=1; % there is a link x->y
+    Pa(x,y)=1/Kout(x); % there is a link x->y
 end
 for i = 1:n3
     x=A2(i,1);
     y=A2(i,2);
-    Pa2(x,y)=1; % there is a link x->y
+    Pa2(x,y)=1/Kout2(x); % there is a link x->y
 end
-for i = 1:n
-    for j=1:n
-        if Pa(i,j)~=0
-            Pa(i,j)=Pa(i,j)/Kout(i);
-        end
-        if Pa2(i,j)~=0
-            Pa2(i,j)=Pa2(i,j)/Kout2(i);
-        end
-    end
-end 
+
 fprintf('maxReal is %d \n', maxreal);
 
 %% third
@@ -80,42 +71,53 @@ NoRealCounter2=n-RealCounter2;
 fprintf('NoRealcounters: %d %d \n',NoRealCounter,NoRealCounter2);
 
 %% extension to work with dangling nodes
-
-d=zeros(n,1);
-d2=d;
-u=zeros(n,1);
-u2=u;
 dangling=0;
 dangling2=0;
-
+d=zeros(double(n),1);
 for i=1:n
     if Kout(i)==0
         d(i,1)=1;
         dangling= dangling+1;
     end
+end
+u=0;
+for i=1:n
+    if d(i,1)==1
+        u=1/dangling;
+    end
+    break;
+end
+clearvars A A2 C C2 file1 fileId x y
+whos 
+
+% du=d*u';
+% clearvars d u
+d2=zeros(double(n),1);
+for i=1:n
     if Kout2(i)==0
         d2(i,1)=1;
         dangling2= dangling2+1;
     end
 end
-
+u2=0;
 for i=1:n
-    if d(i,1)==1
-        u(i,1)=1/dangling;
-    end
     if d2(i,1)==1
-        u2(i,1)=1/dangling2;
+        u2=1/dangling2;
+        break
     end
 end
+ 
+% du2=d2*u2';
+% clearvars d2 u2
 
-if dangling~=0
-    Pa= Pa + d*u';
-end
-
-if dangling2~=0
-    Pa2= Pa2 + d2*u2';
-end
-
+% if dangling~=0
+%     Pa= Pa + du;
+% end
+% 
+% if dangling2~=0
+%     Pa2= Pa2 + du2;
+% end
+whos
 
 %% initialization of personalization vectors
 v=zeros(n,1);
@@ -163,9 +165,9 @@ sum(v2(:,1));
 e=ones(n,1); % e vector
 tol=1e-10;   % tolerance
 pu=(1/(2*n)) * ones(n, 1); % starting values for pagerank
+pu= double(pu);
 pu2=pu; pd=pu; pd2=pu;
 pi=(pu+pu2+pd+pd2)/2;
-pu;
 sum(pi(:,1));
 i=0;
 dp=1;
@@ -180,8 +182,45 @@ while (dp >= tol)   % computation of pagerank, stops when pi converges
     prevpu2=pu2;
     prevpd2=pd2;
     prevpd=pd;
-    pu = [(prevpu'*a)*Pa + prevpu2' + 2*a*prevpd2']'/2;
-    pu2 = [prevpu' + (prevpu2'*a)*Pa2 + 2*a*prevpd2']'/2;
+    test=prevpu'*a;
+    test2=prevpu2'*a;
+    C=zeros(1,n);
+    D=C;
+	for i = 1:1
+        for j = 1:n
+            P=full(Pa(:,j));
+            P2=full(Pa2(:,j));
+            if (d(j)~=1)
+                for k = 1:n
+                    C(i,j) = C(i,j) + test(i,k)*P(k);
+                end
+            else
+                for k= 1:n
+                    if (d(k)==1)
+                        C(i,j) = C(i,j) + test(i,k)*u;
+                    else
+                        C(i,j) = C(i,j) + test(i,k)*P(k);
+                    end
+                end
+            end
+            if (d2(j)~=1)
+                for k = 1:n
+                    D(i,j) = D(i,j) + test2(i,k)*P2(k);
+                end
+            else
+                for k = 1:n
+                    if (d2(k)==1)
+                        D(i,j) = D(i,j) + test2(i,k)*u2;
+                    else
+                        D(i,j) = D(i,j) + test2(i,k)*P2(k);
+                    end
+                end
+            end
+        end
+	end
+
+    pu = [C + prevpu2' + 2*a*prevpd2']'/2;
+    pu2 = [prevpu' + D + 2*a*prevpd2']'/2;
     pd = [(1-a)*(prevpu' + prevpd'*e*v' + prevpd2'*e*v')]'/2;
     pd2 = [(1-a)*(prevpu2' + prevpd'*e*v2' + prevpd2'*e*v2')]'/2;
     pi=(pu+pu2+pd+pd2)/2;
@@ -195,7 +234,7 @@ fprintf('Total number of nodes: %d\n',n);
 fprintf('Total number of links: %d\n',n2+n3);
 fprintf('Total number of iterations: %d\n',i);
 fprintf('Final pagerank:\n')
-disp(pi')
+% disp(pi')
 fprintf('Sum of elements:%f\n',sum(pi));
 fprintf('Final Ranking:\n \tValue: \t  Node:\n')
 
@@ -205,7 +244,7 @@ result=c(1:top);         %top ten
 ind=find(pi>=c(top));    %their indices
 result=flipud(sortrows([pi(ind) ind],1));
 for i=1:top
-    fprintf('\t%f\t%d \n',result(i,1),result(i,2));
+    fprintf('\t%f32\t%d \n',result(i,1),result(i,2));
 end
 
 toc; % end timer
